f7308567c168dbaf2742546531bff07035dbb9af
dorim1209 <50814969+dorim1209@users.noreply.github.com>
2019-09-12 23:58:39 +0900

    3.1

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends React.Component{
     count : 0
   }
   add = () => {
-    console.log("add");
-  }
+    this.setState(current => ({count: current.count + 1}));
+  };
   minus = () => {
-    console.log("minus");
-  }
+    this.setState(current => ({count: current.count - 1}));
+  };
   render(){
     return <div>
       <h1>The number is : {this.state.count}</h1>
