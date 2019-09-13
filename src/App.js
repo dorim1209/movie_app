9bda3f72dc6c72faf6ebfb58c5fe08a595dda273
dorim1209 <50814969+dorim1209@users.noreply.github.com>
2019-09-13 23:00:41 +0900

    4.1 ERR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from 'Movie';
 
-class App extends React.Component{
-  state ={
-    isLoading:true,
-    movies:[]
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-  getMovies = async () =>{
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-  }
-  componentDidMount(){
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading : false });
+  };
+  componentDidMount() {
     this.getMovies();
-    
   }
-
-  
-  render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading.." : "We are ready"}</div>;
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <div>
+      {isLoading 
+        ? "Loading.." 
+        : movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image} 
+            />
+      ))}
+    </div>
+    );
   }
 
 }
