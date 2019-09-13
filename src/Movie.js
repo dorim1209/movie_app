b0b38af25b1d655dfcfd6c8f911c78a6e171128c
dorim1209 <50814969+dorim1209@users.noreply.github.com>
2019-09-14 00:15:02 +0900

    Moive_App 완성

--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,7 @@ function Movie({year, title, summary, poster, genres}){
         <li key ={index} className = "genres_genre">{genre}</li>
         ))}
         </ul>
-        <p className = "moive_summary">{summary}</p>
+        <p className = "moive_summary">{(summary.slice(0,180))}...</p>
         </div>
     </div>
     );
